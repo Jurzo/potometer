4b758fdaa19428a7c628644643515167c4e404e0
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2020-11-13 18:22:17 +0200

    testing

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 
 while 1:
     availableDevices = bleconn.scan()
-    dbDevices = db.getSensors()
+    dbSensors = db.getSensors()
+    dbDevices = [sensor[0] for sensor in dbSensors]
     matches = [device for device in availableDevices if device in dbDevices]
     for match in matches:
         print(match)
