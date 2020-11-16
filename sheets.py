2cd524420eebaa0c939dce63c497cebdcc7a7156
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2020-11-16 18:27:00 +0200

    changed into class

--- sheets.py
+++ sheets.py
@@ -5,56 +5,64 @@ from google.auth.transport.requests import Request
 import os
 import pickle
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+class SheetDriver:
+    def __init__(self):
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        ID = '1kCCLRpFxTEChTQeAWq_ndqPykZ9qz3Ab3mxZC1vQUx0'
+        RANGE = 'A:DDD'
+        rows = 0
+        columns = 0
 
-# here enter the id of your google sheet
-ID = '1kCCLRpFxTEChTQeAWq_ndqPykZ9qz3Ab3mxZC1vQUx0'
-SAMPLE_RANGE_NAME = 'A:DDD'
+    def createService(self):
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES) # here enter the name of your downloaded JSON file
+                creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
 
-def createService():
-    global service
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES) # here enter the name of your downloaded JSON file
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        self.service = build('sheets', 'v4', credentials=creds)
 
-    service = build('sheets', 'v4', credentials=creds)
+    def read(self):
+        # Call the Sheets API
+        sheet = self.service.spreadsheets()
+        result_input = sheet.values().get(spreadsheetId=self.ID, range=self.RANGE).execute()
+        values_input = result_input.get('values', [])
 
-def read():
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result_input = sheet.values().get(spreadsheetId=ID, range=SAMPLE_RANGE_NAME).execute()
-    values_input = result_input.get('values', [])
-    print(len(values_input))
+        self.rows = len(values_input)
+        self.columns = len(values_input[0]-1)
 
-    if not values_input:
-        print('No data found.')
-    return values_input
+        if not values_input:
+            print('No data found.')
+        return values_input
 
-def Export_Data_To_Sheets(df, torange):
-    response_date = service.spreadsheets().values().update(
-        spreadsheetId=ID,
-        valueInputOption='RAW',
-        range=torange,
-        body=dict(
-            majorDimension='ROWS',
-            values=df.T.reset_index().T.values.tolist())
-    ).execute()
-    print('Sheet successfully Updated')
+    def Export_Data_To_Sheets(self, df, torange):
+        self.service.spreadsheets().values().update(
+            spreadsheetId=self.ID,
+            valueInputOption='RAW',
+            range=torange,
+            body=dict(
+                majorDimension='ROWS',
+                values=df.T.reset_index().T.values.tolist())
+        ).execute()
+        print('Sheet successfully Updated')
 
-createService()
-x = read()
+driver = SheetDriver()
+driver.createService()
+print(driver.read())
+print(driver.rows, driver.columns)
 
-df=pd.DataFrame(x[1:], columns=x[0])
-print(df)
-print(x)
+
+#createService()
+#x = read()
+#df=pd.DataFrame(x[1:], columns=x[0])
+#print(df)
+#print(x)
 #Export_Data_To_Sheets(df, 'A2:C12')
