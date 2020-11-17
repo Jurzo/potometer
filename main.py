f542d128ebf6fc84c08838b32a2ee02319c4b0ef
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2020-11-17 13:19:18 +0200

    testing insert to sheet

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import bleconn
 import db
 import time
+import sheets
+import pandas as pd
+
+
+def upload():
+    sheetDriver = sheets.SheetDriver()
+    sheetDriver.createService()
+    sheetDriver.read()
+    if sheetDriver.rows == 0:
+
+        
+
 
 while 1:
     availableDevices = bleconn.scanTool()
     dbSensors = db.getSensors()
+    print(db.getReadings())
     matches = [device for device in dbSensors if device[0] in availableDevices]
     if matches:
         vals = bleconn.readSensors(matches)
