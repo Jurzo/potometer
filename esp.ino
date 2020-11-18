f3144d67f48f928d3c5e0d2b47b92156a05cb64e
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2020-11-18 09:05:12 +0200

    .

--- esp.ino
+++ esp.ino
@@ -3,11 +3,11 @@
 #include <BLEServer.h>
 #include <string.h>
 
-#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  3        /* Time ESP32 will go to sleep (in seconds) */
-#define SERVICE_UUID   "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define SENSOR_UUID    "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-#define SensorPin 34 
+#define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 60          /* Time ESP32 will go to sleep (in seconds) */
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define SENSOR_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SensorPin 34
 #define outPin 2
 
 RTC_DATA_ATTR int bootCount = 0;
@@ -16,32 +16,53 @@ RTC_DATA_ATTR int bootCount = 0;
 Method to print the reason by which ESP32
 has been awaken from sleep
 */
-void print_wakeup_reason(){
+void print_wakeup_reason()
+{
     esp_sleep_wakeup_cause_t wakeup_reason;
 
     wakeup_reason = esp_sleep_get_wakeup_cause();
 
-    switch(wakeup_reason)
+    switch (wakeup_reason)
     {
-        case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-        case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-        case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-        case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-        case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-        default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+    case ESP_SLEEP_WAKEUP_EXT0:
+        Serial.println("Wakeup caused by external signal using RTC_IO");
+        break;
+    case ESP_SLEEP_WAKEUP_EXT1:
+        Serial.println("Wakeup caused by external signal using RTC_CNTL");
+        break;
+    case ESP_SLEEP_WAKEUP_TIMER:
+        Serial.println("Wakeup caused by timer");
+        break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD:
+        Serial.println("Wakeup caused by touchpad");
+        break;
+    case ESP_SLEEP_WAKEUP_ULP:
+        Serial.println("Wakeup caused by ULP program");
+        break;
+    default:
+        Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+        break;
     }
 }
 
-class CharCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic* pCharacteristic) {
+class CharCallbacks : public BLECharacteristicCallbacks
+{
+    void onWrite(BLECharacteristic *pCharacteristic)
+    {
         Serial.println("Char written");
-        Serial.println(millis() / 1000.0);
-        esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-        esp_deep_sleep_start();
+        sleep();
     };
 };
 
-void setup(){
+void sleep()
+{
+    Serial.println(TIME_TO_SLEEP - millis() / 1000.0);
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR - millis() * 1000);
+    esp_deep_sleep_start();
+}
+
+void setup()
+{
     pinMode(outPin, OUTPUT);
     Serial.begin(115200);
     delay(1000); //Take some time to open up the Serial Monitor
@@ -55,26 +76,26 @@ void setup(){
     analogRead(SensorPin);
     float sensorValue = 0;
     digitalWrite(outPin, HIGH);
-    for (int i = 0; i <= 200; i++) 
-    { 
-        sensorValue = sensorValue + analogRead(SensorPin); 
-        delay(2); 
+    for (int i = 0; i <= 200; i++)
+    {
+        sensorValue = sensorValue + analogRead(SensorPin);
+        delay(2);
     }
     digitalWrite(outPin, LOW);
-    sensorValue = sensorValue/200.0;
+    sensorValue = sensorValue / 200.0;
     Serial.println("Sensor value: " + String(sensorValue));
-    int mapped = (int) map(sensorValue,0,2000,0,100);
+    int mapped = (int)map(sensorValue, 0, 2000, 0, 100);
     Serial.println("Mapped value: " + String(mapped));
 
     BLEDevice::init("ESP32-1");
     BLEServer *pServer = BLEDevice::createServer();
     BLEService *pService = pServer->createService(SERVICE_UUID);
     BLECharacteristic *sensorCharacteristic = pService->createCharacteristic(
-                                            SENSOR_UUID,
-                                            BLECharacteristic::PROPERTY_READ |
-                                            BLECharacteristic::PROPERTY_WRITE |
-                                            BLECharacteristic::PROPERTY_NOTIFY
-                                        );
+        SENSOR_UUID,
+        BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE |
+            BLECharacteristic::PROPERTY_NOTIFY);
+
     sensorCharacteristic->setCallbacks(new CharCallbacks());
     sensorCharacteristic->setValue(mapped);
     pService->start();
@@ -82,14 +103,18 @@ void setup(){
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
     pAdvertising->addServiceUUID(SERVICE_UUID);
     pAdvertising->setScanResponse(true);
-    pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+    pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
     pAdvertising->setMinPreferred(0x12);
     BLEDevice::startAdvertising();
     Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
-void loop(){
-  //This is not going to be called
-  delay(1000);
-  Serial.println("waiting...");
+void loop()
+{
+    delay(1000);
+    Serial.println("waiting...");
+    if (millis() / 1000 > TIME_TO_SLEEP / 2)
+    {
+        sleep();
+    }
 }
